c1555c4cbcb4ef62efc864fe273e19ad568e640c
김혜빈 <hyebinkimdesign@gmail.com>
2025-03-16 11:54:08 +0900

    setting: vite,라우터,tailwindcss 설치

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export function Home() {
+  return (
+    <>
+      <header role="banner"></header>
+      <main id="main-content" role="main">
+        <section className="content-section">
+          <h2 className="section-title"></h2>
+          <p>내용을 변경해주세요</p>
+        </section>
+        <article className="content-aricle">
+          <h2 className="aricle-title"></h2>
+        </article>
+      </main>
+      <footer role="contentinfo"></footer>
+    </>
+  );
+}
