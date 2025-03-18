84fa6671b9da1f08f441321bef1aed931a92d9fc
연구개발전담부 <wdpb@mig.kr>
2025-03-18 13:47:45 +0900

    design: home 반응형 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,18 +7,18 @@ export function Home() {
   return (
     <div className="mx-4">
       <main id="main-content" role="main">
-        <section className="content-section w-full mt-[110px] mb-14 ">
-          <figure className="flex flex-wrap max-w-[1200px] gap-5 mx-auto  justify-between items-start">
-            <img className="bg-black w-full max-w-[586px] h-auto rounded-xl" src="/images/mpImage01.png" alt="화장실에서 문제를 분석하고 있는 배수구 전문가" />
+        <section className="content-section w-full mt-[110px] mb-14">
+          <figure className="flex flex-wrap justify-center items-center gap-20  mx-auto max-w-[1200px]">
+            <img className="bg-black w-full sm:max-w-[586px] h-auto rounded-xl" src="/images/mpImage01.png" alt="화장실에서 문제를 분석하고 있는 배수구 전문가" />
 
-            <figcaption className="max-w-353px ">
-              <h2 className="section-title text-[24px] font-semibold">
+            <figcaption className="max-w-[353px] w-full">
+              <h2 className="section-title text-[24px] sm:text-[28px] font-semibold">
                 안녕하세요!
                 <br />
                 24시간 송파구 문정동 배관 전문 서비스
               </h2>
               <br />
-              <p className="text-[16px] font-medium">
+              <p className="text-[16px] sm:text-[18px] font-medium">
                 싱크대 · 하수구 · 변기 막힘
                 <br /> 고압세척 · 관로탐지 · 배관 내시경
                 <br /> 배관 문제, 빠르고 확실하게 해결해 드립니다! <br />
@@ -33,12 +33,11 @@ export function Home() {
           </figure>
         </section>
 
-        {/*  */}
-        <section className="content-section flex flex-wrap gap-6 mx-auto w-full max-w-[1200px] justify-center border-t-2 py-16 ">
-          <img className="w-[282px] h-[282px]" src="/images/imgListItem01.png" alt="막힌 하수구에 카메라를 넣고 있는 전문가" />
-          <img className="w-[282px] h-[282px]" src="/images/imgListItem02.png" alt="화장실 하수구를 뚫고 있는 상황" />
-          <img className="w-[282px] h-[282px]" src="/images/imgListItem03.png" alt="전문 기계" />
-          <img className="w-[282px] h-[282px]" src="/images/imgListItem04.png" alt="막힌 하수구에 전문기계를 사용하고 있는 전문가" />
+        <section className="content-section flex flex-wrap gap-6 mx-auto w-full max-w-[1200px] justify-center border-t-2 py-16">
+          <img className="w-[282px] h-[282px] sm:w-[200px] sm:h-[200px]" src="/images/imgListItem01.png" alt="막힌 하수구에 카메라를 넣고 있는 전문가" />
+          <img className="w-[282px] h-[282px] sm:w-[200px] sm:h-[200px]" src="/images/imgListItem02.png" alt="화장실 하수구를 뚫고 있는 상황" />
+          <img className="w-[282px] h-[282px] sm:w-[200px] sm:h-[200px]" src="/images/imgListItem03.png" alt="전문 기계" />
+          <img className="w-[282px] h-[282px] sm:w-[200px] sm:h-[200px]" src="/images/imgListItem04.png" alt="막힌 하수구에 전문기계를 사용하고 있는 전문가" />
         </section>
       </main>
     </div>
