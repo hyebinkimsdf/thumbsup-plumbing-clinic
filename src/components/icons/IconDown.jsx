0578705aba7d7876b709665be97e16776f161edb
연구개발전담부 <wdpb@mig.kr>
2025-03-20 10:20:19 +0900

    design: ProcessPage 반응형 구현, svg 컴포넌트 추가

---
+++ src/components/icons/IconDown.jsx
@@ -0,0 +1,9 @@
+export function IconDown() {
+  return (
+    <>
+      <svg width="26" height="15" viewBox="0 0 26 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M25 1L12.8209 13L1 0.999999" stroke="#888888" stroke-width="2" stroke-linecap="round" />
+      </svg>
+    </>
+  );
+}
--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,10 +1,10 @@
 import { Ask } from "../components/Ask";
-import { Figure } from "../components/Figure";
+import { Figure01 } from "../components/Figure01";
 
 export function ServicesPage() {
   return (
     <div className="flex flex-col gap-10">
-      <Figure
+      <Figure01
         categoryTitle="서비스 01"
         contentTitle="변기 교체"
         contentDescription="고압 세척은 강력한 수압으로 하수구 내부의 찌꺼기와 이물질을 제거하여 배수 문제를 해결하고, 배관 내시경은 실시간으로 배관 내부를 확인해 정확한 막힘 위치를 파악하여 신속하게 조치합니다. 또한, 흡입 & 압축 장비는 변기나 싱크대의 하수구 막힘을 빠르게 제거하고 원활한 배수 시스템을 복원하는 데 도움을 줍니다."
@@ -14,7 +14,7 @@ export function ServicesPage() {
         direction="row"
         textAlignment="left"
       />
-      <Figure
+      <Figure01
         categoryTitle="서비스 02"
         contentTitle="변기 교체"
         contentDescription="고압 세척은 강력한 수압으로 하수구 내부의 찌꺼기와 이물질을 제거하여 배수 문제를 해결하고, 배관 내시경은 실시간으로 배관 내부를 확인해 정확한 막힘 위치를 파악하여 신속하게 조치합니다. 또한, 흡입 & 압축 장비는 변기나 싱크대의 하수구 막힘을 빠르게 제거하고 원활한 배수 시스템을 복원하는 데 도움을 줍니다."
@@ -24,7 +24,7 @@ export function ServicesPage() {
         direction="row-reverse"
         textAlignment="right"
       />
-      <Figure
+      <Figure01
         categoryTitle="서비스 03"
         contentTitle="변기 교체"
         contentDescription="고압 세척은 강력한 수압으로 하수구 내부의 찌꺼기와 이물질을 제거하여 배수 문제를 해결하고, 배관 내시경은 실시간으로 배관 내부를 확인해 정확한 막힘 위치를 파악하여 신속하게 조치합니다. 또한, 흡입 & 압축 장비는 변기나 싱크대의 하수구 막힘을 빠르게 제거하고 원활한 배수 시스템을 복원하는 데 도움을 줍니다."
@@ -33,7 +33,7 @@ export function ServicesPage() {
         alignment="end"
         direction="row"
       />
-      <Figure
+      <Figure01
         categoryTitle="서비스 04"
         contentTitle="변기 교체"
         contentDescription="고압 세척은 강력한 수압으로 하수구 내부의 찌꺼기와 이물질을 제거하여 배수 문제를 해결하고, 배관 내시경은 실시간으로 배관 내부를 확인해 정확한 막힘 위치를 파악하여 신속하게 조치합니다. 또한, 흡입 & 압축 장비는 변기나 싱크대의 하수구 막힘을 빠르게 제거하고 원활한 배수 시스템을 복원하는 데 도움을 줍니다."
