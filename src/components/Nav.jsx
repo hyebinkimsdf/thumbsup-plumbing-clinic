e2d82006d36e7369d0cfe73c0eeffb022cffd180
연구개발전담부 <wdpb@mig.kr>
2025-03-26 09:25:32 +0900

    fix: 반응형 Nav 고정값 수정

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,58 +8,59 @@ export function Nav() {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // 활성화 상태에 적용할 클래스
   const activeClassName = "border-b-2 border-[#18A3FA] font-semibold text-[#18A3FA]";
 
   return (
     <>
-      <nav aria-label="주요 메뉴" role="navigation" className="flex gap-2 w-full max-w-[1200px] h-16 px-4 mx-auto mb-10 md:mb-24 items-end justify-center md:justify-between">
-        {/* 회사명 - 모바일에서 메뉴 열리면 가운데 정렬 */}
-        <Link to={"/"}>
-          <p className="text-3xl font-semibold">엄지척배관클리닉</p>
-        </Link>
+      <nav aria-label="주요 메뉴" role="navigation" className="flex gap-2 w-full  h-20 px-4 mx-auto mb-10 md:mb-24 items-center justify-center md:justify-between fixed z-20 top-0  bg-white shadow-sm">
+        <div className="max-w-[1200px] flex justify-center md:justify-between w-full mx-auto">
+          {/* 회사명 */}
+          <Link to={"/"}>
+            <p className="text-3xl font-semibold">엄지척배관클리닉</p>
+          </Link>
 
-        {/* Mobile menu button */}
-        <button className="md:hidden flex flex-col fixed right-8 top-10" onClick={toggleMenu} aria-expanded={isMenuOpen} aria-controls="mobile-menu" aria-label="메뉴 열기">
-          <span className="block w-6 h-0.5 bg-black mb-1"></span>
-          <span className="block w-6 h-0.5 bg-black mb-1"></span>
-          <span className="block w-6 h-0.5 bg-black"></span>
-        </button>
+          {/* Mobile menu button */}
+          <button className="md:hidden flex flex-col fixed right-8 top-10" onClick={toggleMenu} aria-expanded={isMenuOpen} aria-controls="mobile-menu" aria-label="메뉴 열기">
+            <span className="block w-6 h-0.5 bg-black mb-1"></span>
+            <span className="block w-6 h-0.5 bg-black mb-1"></span>
+            <span className="block w-6 h-0.5 bg-black"></span>
+          </button>
 
-        {/* Desktop menu */}
-        <ul className="nav-list hidden md:flex gap-10 items-center ">
-          <li className="nav-item">
-            <NavLink to="/" className={({ isActive }) => (isActive ? activeClassName : "")} end>
-              회사소개
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/machines" className={({ isActive }) => (isActive ? activeClassName : "")}>
-              장비소개
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/services" className={({ isActive }) => (isActive ? activeClassName : "")}>
-              서비스품목
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/process" className={({ isActive }) => (isActive ? activeClassName : "")}>
-              서비스절차
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/gallery" className={({ isActive }) => (isActive ? activeClassName : "")}>
-              영상&사진
-            </NavLink>
-          </li>
-        </ul>
+          {/* Desktop menu */}
+          <ul className="nav-list hidden md:flex gap-10 items-center">
+            <li className="nav-item">
+              <NavLink to="/" className={({ isActive }) => (isActive ? activeClassName : "")} end>
+                회사소개
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/machines" className={({ isActive }) => (isActive ? activeClassName : "")}>
+                장비소개
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/services" className={({ isActive }) => (isActive ? activeClassName : "")}>
+                서비스품목
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/process" className={({ isActive }) => (isActive ? activeClassName : "")}>
+                서비스절차
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/gallery" className={({ isActive }) => (isActive ? activeClassName : "")}>
+                영상&사진
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
 
-      {/* Mobile menu */}
+      {/* Mobile menu - 모바일에서만 상단 고정 */}
       {isMenuOpen && (
-        <div id="mobile-menu" className="md:hidden w-full bg-white shadow-md pb-10">
-          <ul className="flex flex-col items-center gap-4">
+        <div id="mobile-menu" className="md:hidden fixed top-14 h-80 left-0 w-full bg-white shadow-md pb-12 pt-10 z-10 ">
+          <ul className="flex flex-col items-center justify-center gap-4">
             <li className="nav-item py-2">
               <NavLink to="/" className={({ isActive }) => (isActive ? activeClassName : "")} onClick={toggleMenu} end>
                 회사소개
